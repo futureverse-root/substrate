2391379d11fcb4f2f5cd31ca5dd4e4260f21e6ce
JasonTulp <jason.j.tulp@gmail.com>
2023-10-19 18:43:32 +1100

    Update time to 9.15pm NZT

--- primitives/timestamp/src/lib.rs
+++ primitives/timestamp/src/lib.rs
@@ -239,7 +239,7 @@ impl sp_inherents::InherentDataProvider for InherentDataProvider {
 		// TRN HOTFIX: mutate timestamp to make it revert back in time and have slots
 		// happen at 6x their speed from then until we have caught up with the present time.
 
-		const REVIVE_TIMESTAMP: u64 = 1697701500000; // Thurs 19, Oct 2023 8.45pm NZT
+		const REVIVE_TIMESTAMP: u64 = 1697703300000; // Thurs 19, Oct 2023 8.45pm NZT
 		const FORK_TIMESTAMP: u64 = 1697575576000; // Block number 8,260,344 18/10/2023, 07:46:16
 		const WARP_FACTOR: u64 = 2;
 
