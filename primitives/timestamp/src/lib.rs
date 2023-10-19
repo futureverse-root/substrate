e6ce413daccc225453005f2841a116686c7c99fd
JasonTulp <jason.j.tulp@gmail.com>
2023-10-19 20:55:33 +1100

    Update time to 11.45pm NZT block 344

--- primitives/timestamp/src/lib.rs
+++ primitives/timestamp/src/lib.rs
@@ -206,7 +206,7 @@ impl InherentDataProvider {
 		// TRN HOTFIX: mutate timestamp to make it revert back in time and have slots
 		// happen at 6x their speed from then until we have caught up with the present time.
 
-		const REVIVE_TIMESTAMP: u64 = 1697708700000; // Thurs 19, Oct 2023 10.45pm NZT
+		const REVIVE_TIMESTAMP: u64 = 1697712300000; // Thurs 19, Oct 2023 11.45pm NZT
 		const FORK_TIMESTAMP: u64 = 1697575576000; // Block number 8,260,344 18/10/2023, 07:46:16
 		const WARP_FACTOR: u64 = 2;
 
